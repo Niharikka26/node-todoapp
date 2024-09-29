ea0468cdcc395a5a972ce53e06b665f794cd0ae5
Niharikka26 <nv26112000@gmail.com>
2024-09-30 03:16:52 +0530

    minor changes1

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import {connectDB} from "./data/database.js";
 
 
 connectDB();
+const port = process.env.PORT || 4000;
 
-app.listen(process.env.PORT,()=>{
-  console.log(`Server is working on port : ${process.env.PORT} in  ${process.env.NODE_ENV} mode`);
+app.listen(port,()=>{
+  console.log(`Server is working on port : ${port} in  ${process.env.NODE_ENV} mode`);
 });
