51ca930d944920516107968fdb6ffb968a4233a8
Niharikka26 <69249036+Niharikka26@users.noreply.github.com>
2024-09-30 22:04:03 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -95,7 +95,7 @@ import ErrorHandler from "../middlewares/error.js";
   };
 
   export const logout=(req,res)=>{
-   res.status(404).cookie("token"," ",{
+   res.status(200).cookie("token"," ",{
     expires: new Date(Date.now()),
     sameSite: process.env.NODE_ENV==="Development"? "lax":"none",
     secure: process.env.NODE_ENV==="Development"? false:true,
